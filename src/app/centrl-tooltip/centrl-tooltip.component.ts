c541f1234e8967301c83f001b25a159e09827cde
Rafay Choudhury <rchoudhury@oncentrl.com>
2017-12-21 19:16:05 -0800

    mouseevents are properly in place, allowing the tooltip to be visible while the cursor is over it

--- src/app/centrl-tooltip/centrl-tooltip.component.ts
+++ src/app/centrl-tooltip/centrl-tooltip.component.ts
@@ -1,17 +1,31 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, HostListener, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'centrl-tooltip',
   template: `
     <div class="tooltip-container">
-      <div class="text">
-        <p>{{ text }}</p>
-        <div class="tooltip">{{ text }}</div>
+      <p>{{ text }}</p>
+      <div #toolTip
+           (mouseleave)="onMouseLeave()"
+           class="tooltip">{{ text }}
       </div>
     </div>
   `,
   styleUrls: ['centrl-tooltip.component.scss']
 })
 export class CentrlTooltipComponent {
+  @ViewChild('toolTip') toolTip: ElementRef;
   @Input() public text: string;
+
+  constructor(private el: ElementRef) {
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.toolTip.nativeElement.style.visibility = 'visible';
+  }
+
+  onMouseLeave() {
+    this.toolTip.nativeElement.style.visibility = 'hidden';
+  }
 }
